051144c4d0545070759c91eea510158c95e30f26
Oumar Barry <oumar.barry1202@gmail.com>
2024-05-19 03:29:11 +0000

    fix: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
+  schema: "./server/db/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: { url: process.env.DB_URL },
